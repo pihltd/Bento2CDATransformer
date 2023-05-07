5d2552fff8ba04775c11cc6180833fbf854bcbff
Todd Pihl <todd.pihl@gmail.com>
2023-05-07 13:44:06 -0400

    Adding src and test folders

--- cdsQueries.py
+++ cdsQueries.py
@@ -10,24 +10,105 @@ def init():
     
     global file_info
     file_info ="""
-{
-file{
-    file_id
-    file_name
-    file_type
-    file_url_in_cds
-    file_size
-    md5sum
-    study{
-      phs_accession
-      study_name
-      study_acronym
-    }
-    samples{
-        sample_id
+    {
+        file{
+            file_id
+            file_name
+            file_type
+            file_size
+            md5sum
+        }
+    }"""
+
+    global file_samples
+    file_samples = """
+    {
+        file{
+            file_id
+            samples{
+                sample_id
+            }
+        }
+    }"""
+    global file_participants
+    file_participants = """
+    {
+        file{
+            file_id
+            samples{
+            participant{
+                participant_id
+            }
+            }
+        }
+    }"""
+    global diagnosis_info
+    diagnosis_info = """
+    {
+        diagnosis{
+            diagnosis_id
+            primary_diagnosis
+            age_at_diagnosis
+            morphology
+            tumor_stage_clinical_m
+            tumor_stage_clinical_n
+            tumor_stage_clinical_t
+            tumor_grade
+        }
+    }"""
+
+    global treatment_info
+    treatment_info = """
+    {
+        treatment{
+            treatment_id
+            treatment_type
+            treatment_outcome
+            days_to_treatment
+            therapeutic_agents
+        }
+    }"""
+
+    global sample_info
+    sample_info = """
+    {
+        sample{
+            sample_id
+            sample_age_at_collection
+            sample_anatomic_site
+            sample_type
+            derived_from_specimen
+        }
+    }"""
+
+    global basic_subject_info
+    basic_subject_info = """
+    {
         participant{
             participant_id
-      }
+            gender
+            race
+            ethnicity
+        }
     }
-}
-}"""
+    """
+
+    global test_subject_info
+    test_subject_info = """
+    {
+        participant{
+            participant_id
+            gender
+            race
+            ethnicity
+            study{
+                organism_species
+                study_acronym
+            }
+            diagnoses{
+                vital_status
+            }
+        }
+    }
+    """
+
