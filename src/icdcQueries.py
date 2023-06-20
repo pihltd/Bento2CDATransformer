c5885ed7076725e87b273e5fe344288607c0c259
Todd Pihl <pihltd@users.noreply.github.com>
2023-06-20 14:08:55 -0400

    Update icdcQueries.py
    Added example query from Ming

--- src/icdcQueries.py
+++ src/icdcQueries.py
@@ -1,3 +1,35 @@
 def init():
-    global file_info
+    global ming_query
+    ming_qure = """{
+  file {
+    uuid
+    file_name
+    file_type
+    file_size
+    file_format
+    md5sum
+    file_location
+    study {
+      clinical_study_designation
+    }
+    case{
+      case_id
+      canine_individual {
+        canine_individual_id
+      }
+    }
+    diagnosis {
+      diagnosis_id
+      case {
+        case_id
+      }
+    }
+    sample {
+      sample_id
+      case {
+          case_id
+      }
+    }
+  }
+}"""
     
